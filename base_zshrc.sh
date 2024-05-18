da9d38c8e336fd7ba6232bcf8da726574058363a
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-05-17 19:49:06 -0700

    initial commit

---
+++ base_zshrc.sh
@@ -0,0 +1,14 @@
+PROMPT='%b%F{#0b98de}%~ %f
+%? %F{#fd9014}%(!.#.>) %f'
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+
+# aliases
+alias ls='ls --color'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias vim='nvim'
+alias c='clear'
+
